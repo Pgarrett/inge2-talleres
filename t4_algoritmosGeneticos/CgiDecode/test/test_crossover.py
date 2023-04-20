06729ead3fdc5b4d860bf34016a527d5f50bf388
Pgarrett <philip.garrett@mulesoft.com>
2023-04-20 18:21:14 -0300

    ejercicio 8 completo

--- t4_algoritmosGeneticos/CgiDecode/test/test_crossover.py
+++ t4_algoritmosGeneticos/CgiDecode/test/test_crossover.py
@@ -1,23 +1,27 @@
 #!./venv/bin/python
+import random
 import unittest
 
 from src.create_population import create_population
 from src.crossover import crossover
+from src.evaluate_condition import clear_maps
 
 
 class TestCrossover(unittest.TestCase):
 
-    def testWithPredefinedPopulation(self):
-        population = [["Hello+Reader", "", "Hello+R%eader", "+"], ["%AA", "%1", "&&&"]]
-        offspring1, offspring2 = crossover(population[0], population[1], 2)
-        print(offspring1)
-        print(offspring2)
-        # self.assertEqual(winnerIndex, 0)
-        # self.assertEqual(winnerScore, 1)
+    def setUp(self):
+        clear_maps()
 
-    def testExample(self):
+    def testWithPredefinedPopulation(self):
+        population = [["Hello+Reader", "", "Hello+R%eader", "+"], ["%AA", "%1", "&&&", "%AU", "1", "2432sa145.l", "[]", "qerasdfb", "12345678"]]
+        offspring1, offspring2 = crossover(population[0], population[1], 2)
+        crossoverLimit = random.randint(1, min(len(population[0]), len(population[1])))
+        self.assertEqual(offspring1, population[0][0:crossoverLimit] + population[1][crossoverLimit:])
+        self.assertEqual(offspring2, population[1][0:crossoverLimit] + population[0][crossoverLimit:])
+
+    def testRandomPopulation(self):
         population = create_population(2)
-        crossover(population[0], population[1])
-        self.assertTrue(True)
-        self.assertFalse(False)
-        self.assertEqual(True, True)
+        offspring1, offspring2 = crossover(population[0], population[1], 2)
+        crossoverLimit = random.randint(1, min(len(population[0]), len(population[1])))
+        self.assertEqual(offspring1, population[0][0:crossoverLimit] + population[1][crossoverLimit:])
+        self.assertEqual(offspring2, population[1][0:crossoverLimit] + population[0][crossoverLimit:])
